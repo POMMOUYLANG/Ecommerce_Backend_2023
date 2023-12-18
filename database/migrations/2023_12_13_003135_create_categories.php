8638e6756ad565c0773bc4432887b797c573f6fc
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-12-18 14:41:16 +0700

    change_database

--- database/migrations/2023_12_13_003135_create_categories.php
+++ database/migrations/2023_12_13_003135_create_categories.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
+            $table->timestamps();
         });
     }
 
